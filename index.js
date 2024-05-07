d7a7eb3e819eebca7ff846a720e81dc780451d94
Dajunctic Vu <59690194+Dajunctic@users.noreply.github.com>
2024-05-07 13:24:20 +0700

    Update homepage

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const typed = new Typed('#multiple', {
+	strings: ['Lập trình viên', 'Youtuber', 'Blogger' ,'Họa sĩ châu Á'],
+	typeSpeed: 200,
+	backSpeed: 200,
+	backDelay: 1000,
+	loop: True
+});
