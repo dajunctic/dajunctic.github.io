7db0fa57d1421d0005b819aa064fb02e363eb45e
Dajunctic Vu <59690194+Dajunctic@users.noreply.github.com>
2024-05-09 13:20:37 +0700

    Update responsive navbar

--- index.js
+++
@@ -1,8 +0,0 @@
-
-const typed = new Typed('#multiple', {
-	strings: ['Lập trình viên', 'Youtuber', 'Blogger' ,'Họa sĩ châu Á'],
-	typeSpeed: 200,
-	backSpeed: 200,
-	backDelay: 1000,
-	loop: True
-});
